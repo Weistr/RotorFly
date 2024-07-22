8aa301f4161dd7ec4cad197ef9ab968d509809e6
Weistr <1561267515@qq.com>
2024-07-22 23:09:56 +0800

    temp

---
+++ V2_GD/firmware/CMakeFiles/RotorFly_fw.elf.dir/App/gd32f3x0_it.c.obj.d
@@ -0,0 +1,12 @@
+CMakeFiles/RotorFly_fw.elf.dir/App/gd32f3x0_it.c.obj: \
+ C:\Users\15612\Documents\Project\RotorFly\V2_GD\firmware\App\gd32f3x0_it.c \
+ C:\Users\15612\Documents\Project\RotorFly\V2_GD\firmware\App\Inc/gd32f3x0_it.h \
+ C:\Users\15612\Documents\Project\RotorFly\V2_GD\firmware\App\Inc/main.h \
+ C:\Users\15612\Documents\Project\RotorFly\V2_GD\firmware\App\Inc/systick.h \
+ c:\tools\gcc_arm\arm-gnu-toolchain-12.3.rel1-mingw-w64-i686-arm-none-eabi\lib\gcc\arm-none-eabi\12.3.1\include\stdint.h \
+ c:\tools\gcc_arm\arm-gnu-toolchain-12.3.rel1-mingw-w64-i686-arm-none-eabi\arm-none-eabi\include\stdint.h \
+ c:\tools\gcc_arm\arm-gnu-toolchain-12.3.rel1-mingw-w64-i686-arm-none-eabi\arm-none-eabi\include\machine\_default_types.h \
+ c:\tools\gcc_arm\arm-gnu-toolchain-12.3.rel1-mingw-w64-i686-arm-none-eabi\arm-none-eabi\include\sys\features.h \
+ c:\tools\gcc_arm\arm-gnu-toolchain-12.3.rel1-mingw-w64-i686-arm-none-eabi\arm-none-eabi\include\_newlib_version.h \
+ c:\tools\gcc_arm\arm-gnu-toolchain-12.3.rel1-mingw-w64-i686-arm-none-eabi\arm-none-eabi\include\sys\_intsup.h \
+ c:\tools\gcc_arm\arm-gnu-toolchain-12.3.rel1-mingw-w64-i686-arm-none-eabi\arm-none-eabi\include\sys\_stdint.h
